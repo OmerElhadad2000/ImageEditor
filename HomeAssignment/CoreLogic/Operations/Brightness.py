c611dc9120a2634f3df6e3c64d47b173edfbaa30
omerelhadad2000 <omer.elh11@gmail.com>
2025-05-05 12:43:58 +0300

    Adding Brightness apply function documentation

--- HomeAssignment/CoreLogic/Operations/Brightness.py
+++ HomeAssignment/CoreLogic/Operations/Brightness.py
@@ -1,23 +1,30 @@
 import numpy as np
 
 from CoreLogic.Operations.base_operation import BaseOperation
-from Infrastructure.Utils import Utils
 
 
 class Brightness(BaseOperation):
     def __init__(self):
         """
         Initialize the Brightness operation.
-        :param filter_amount: The brightness adjustment value.
         """
         self.filter_amount = 0
+
     def set_filter_amount(self, filter_amount: float):
         """
         Set the brightness adjustment value.
         :param filter_amount: The brightness adjustment value.
         """
         self.filter_amount = filter_amount
+
     def apply(self, image: np.ndarray) -> np.ndarray:
+        """
+        Apply the brightness adjustment to the image, by adding a scalar value to each pixel.
+        We convert the image to float64 to avoid overflow and underflow issues.
+        The image is then clipped to the range [0, 255] and converted back to undefined int fitting pixel def.
+        :param image:
+        :return:
+        """
         image = image.astype(np.float64)
         image += self.filter_amount
         image = np.clip(image, 0, 255)
