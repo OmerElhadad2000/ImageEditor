db63b9371379faf5ae7cf1403d2376e56c6d9e12
omerelhadad2000 <omer.elh11@gmail.com>
2025-05-05 11:53:03 +0300

    Adding image IO operations

--- HomeAssignment/CoreLogic/Operations/base_operation.py
+++ HomeAssignment/CoreLogic/Operations/base_operation.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-import np
+import numpy as np
 
 
 class BaseOperation(ABC):
---
+++ HomeAssignment/Infrastructure/ImageIO.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import numpy as np
+
+class ImageIO:
+    """
+    A class for reading and writing images.
+    """
+
+    @staticmethod
+    def read_image(image_path: str) -> np.ndarray:
+        """
+        Read an image from a file.
+
+        :param image_path: The path to the image file.
+        :return: The image as a NumPy array.
+        """
+        with Image.open(image_path) as img:
+            return np.array(img)
+
+    @staticmethod
+    def write_image(image: np.ndarray, output_path: str) -> None:
+        """
+        Write an image to a file.
+
+        :param image: The image as a NumPy array.
+        :param output_path: The path where the image will be saved.
+        """
+        img = Image.fromarray(image)
+        img.save(output_path)
