e17cf915dad3ddb132452e3277e7d2ec3156620c
omerelhadad2000 <omer.elh11@gmail.com>
2025-05-05 11:55:51 +0300

    Changing the abstract class of BaseOperation so the apply doesnt require a float of change (some filters might not need it and can be changed on init)

--- HomeAssignment/CoreLogic/Operations/base_operation.py
+++ HomeAssignment/CoreLogic/Operations/base_operation.py
@@ -10,7 +10,6 @@ class BaseOperation(ABC):
     def apply(self, image: np.ndarray) -> np.ndarray:
         """
         Apply the operation to the image.
-
         :param image: The image to which the operation will be applied.
         :return: The modified image.
         """
